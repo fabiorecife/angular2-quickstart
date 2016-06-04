7efcc199d9e45ec437b9b5d7b9027dce4cf98a39
fabiorecife <fabiorecife@gmail.com>
2016-06-04 12:31:36 -0300

    version 4 jun 2016 . angular\quickstart

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,7 @@
-import {Component} from 'angular2/core';
+import {Component} from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App - beta 17 - by Fabio</h1>'
+    template: '<h1>My First Angular 2 App</h1>'
 })
 export class AppComponent { }
